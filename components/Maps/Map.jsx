3ea8a89b958bdf30a2ae45228adce4aebfc47ea4
Dheeraj TP <dheerajtp6338@gmail.com>
2024-06-17 23:40:33 +0530

    :sparkles: Shapes added

--- components/Maps/Map.jsx
+++ components/Maps/Map.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
-import { MapView, Camera, LocationPuck } from '@rnmapbox/maps';
+import { MapView, Camera, LocationPuck, ShapeSource, SymbolLayer, Images } from '@rnmapbox/maps';
 import useMapbox from '../../hooks/useMapBox';
 import { Alert } from 'react-native';
 import Error from './Error';
+import { featureCollection, point } from '@turf/helpers';
+import pin from '../../assets/motorbike.png';
 
 const Map = () => {
   const { accessToken, locationPermission } = useMapbox();
@@ -17,8 +19,18 @@ const Map = () => {
 
   return (
     <MapView style={{ flex: 1 }} styleURL="mapbox://styles/mapbox/dark-v11">
-      <Camera followUserLocation />
+      <Camera followUserLocation followZoomLevel={10} />
       <LocationPuck pulsing={{ isEnabled: true }} />
+      <ShapeSource
+        id="scooters"
+        shape={featureCollection([point([41.3907, 2.1589]), point([2.1589, 41.3907])])}>
+        <SymbolLayer
+          id="scooter-icons"
+          minZoomLevel={1}
+          style={{ iconImage: 'pin', iconAllowOverlap: true, iconSize: 0.05, iconAnchor: 'bottom' }}
+        />
+        <Images images={{ pin }} />
+      </ShapeSource>
     </MapView>
   );
 };
